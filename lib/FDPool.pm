7fa54efb46d7ebec707ad1f172f12151293cf74c
Toru Yamaguchi <zigorou@cpan.org>
2011-10-27 01:28:22 +0900

    initial commit

---
+++ lib/FDPool.pm
@@ -0,0 +1,32 @@
+package FDPool;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+FDPool -
+
+=head1 SYNOPSIS
+
+  use FDPool;
+
+=head1 DESCRIPTION
+
+FDPool is
+
+=head1 AUTHOR
+
+Toru Yamaguchi E<lt>zigorou@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
